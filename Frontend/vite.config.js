977720b0521a89f507277573ec09abbaca6c452d
Surajkumar0806 <kottalsurajkumar8@gmail.com>
2025-08-29 18:44:51 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/blockchaintest/',  // Replace with your actual GitHub repository name
+  build: {
+    outDir: 'dist'
+  }
 })
